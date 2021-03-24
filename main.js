94a3d6cc2e478148b521e5e7e56582f6996b1c71
jeremybarbosa <jbarbosa1430@gmail.com>
2021-03-23 19:48:24 -0500

    hue

---
+++ main.js
@@ -0,0 +1,5 @@
+// night mode========================================
+const newBG = document.getElementById('#nightMode');
+newBG.addEventListener('click', function(){
+    document.getElementById('#container').style.backgroundImage = "url('https://media.giphy.com/media/aZ3LDBs1ExsE8/giphy.gif')";
+});
