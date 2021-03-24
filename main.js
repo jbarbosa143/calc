c7abf1bb0fd44880f167045dbcb19971d49c335c
jeremybarbosa <jbarbosa1430@gmail.com>
2021-03-24 08:54:18 -0500

    got nightmode to work

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // night mode========================================
-const newBG = document.getElementById('#nightMode');
+const newBG = document.getElementById('nightMode');
 newBG.addEventListener('click', function(){
-    document.getElementById('#container').style.backgroundImage = "url('https://media.giphy.com/media/aZ3LDBs1ExsE8/giphy.gif')";
+    document.querySelector('#container').style.backgroundImage = "url('https://media.giphy.com/media/aZ3LDBs1ExsE8/giphy.gif')";
+    console.log('nightmode clicked');
 });
