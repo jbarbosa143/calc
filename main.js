e1f780369f008c2ee367731a60cdb793aad03f62
jeremybarbosa <jbarbosa1430@gmail.com>
2021-03-24 09:05:15 -0500

    night and day mode added

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 // night mode========================================
-const newBG = document.getElementById('nightMode');
-newBG.addEventListener('click', function(){
+const nightM = document.getElementById('nightMode');
+nightM.addEventListener('click', function(){
     document.querySelector('#container').style.backgroundImage = "url('https://media.giphy.com/media/aZ3LDBs1ExsE8/giphy.gif')";
     console.log('nightmode clicked');
 });
+
+// day mode =======================================
+const dayM = document.getElementById('daymode');
+dayM.addEventListener('click', function(){
+    document.querySelector('#container').style.backgroundImage ="url('https://media.giphy.com/media/qCfQ28HLs61r2/giphy.gif')";
+}) 
